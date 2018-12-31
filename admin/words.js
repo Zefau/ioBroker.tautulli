32e84324ccab42d5c54796d9b15d9b5f4a273014
Zefau <Zefau@users.noreply.github.com>
2018-12-31 16:57:52 +0100

    Add files via upload

---
+++ admin/words.js
@@ -0,0 +1,54 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	"tab_config": {
+		"en": "Configuration",
+		"de": "Einstellungen"
+	},
+	"tab_alexa": {
+		"en": "Alexa integration",
+		"de": "Alexa Integration"
+	},
+	"tab_info": {
+		"en": "Information",
+		"de": "Informationen"
+	},
+	
+	"events": {
+		"en": "Subscribe / listen to events (Configuration of the webhook)",
+		"de": "Events abonnieren (Konfiguration des Webhooks)"
+	},
+	"events_info": {
+		"en": "Listening to events allows you to receive events from Plex via Tautulli.",
+		"de": "Events abonnieren ermöglichen es, Notifications von Plex via Tautulli zu empfangen."
+	},
+	"webhook_iobroker": {
+		"en": "ioBroker.cloud/iot (via Internet)",
+		"de": "ioBroker.cloud/iot (über das Internet)"
+	},
+	"webhook_internal": {
+		"en": "Webhook (via internal network)",
+		"de": "Webhook (über internes Netzwerk)"
+	},
+	"iobroker_info": {
+		"en": "Use iobroker.cloud or iobroker.iot Adapter to receive events from Tautulli. <a target='_blank' href='https://github.com/Zefau/ioBroker.tautulli#advanced-setup'>See setup instructions here</a>.",
+		"de": "Benutze den iobroker.cloud oder iobroker.iot Adapter, um Events von Tautulli zu empfangen. <a target='_blank' href='https://github.com/Zefau/ioBroker.tautulli/blob/master/README.de.md#erweiterte-installation'>Siehe Anleitung zur Installation hier</a>."
+	},
+	"iobroker": {
+		"en": "ioBroker.iot Service URL",
+		"de": "ioBroker.iot Service URL"
+	},
+	"iot": {
+		"en": "ioBroker.iot Tautulli State",
+		"de": "ioBroker.iot Tautulli Objekt"
+	},
+	"internal_info": {
+		"en": "ioBroker.tautulli will always listen for events from Tautulli within the internal network.",
+		"de": "ioBroker.tautulli empfängt immer über das interne Netzwerk Benachrichtigungen von Tautulli."
+	},
+	"port": {
+		"en": "Port",
+		"de": "Port"
+	},
+};
